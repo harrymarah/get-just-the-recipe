a0df5fef9b065fe5ac60ec37b1fb8e44f49ce8fd
Harry Marah <info@harrymarah.com>
2022-09-21 14:37:06 +0100

    Wrote logic to map over the array of uls to insert into a div and render. Added very basic styles to restrict the size of the divs as many uls take up a lot of room. Planning to swap divs to a react component to use to display the uls.

--- src/components/ShowAllUls.js
+++ src/components/ShowAllUls.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import './ShowAllUls.css'
 
 const ShowAllUls = (props) => {
-    const html = props.allRecipeUls[33]
-    console.log(props.allRecipeUls[33])
-    // const returnHtml = () => {
-    //     return {__html: html}
-    // }    
+    const uls = props.allRecipeUls.map(ul => {
+        return <div key={uuidv4()}>
+            <ul className='single-ul' dangerouslySetInnerHTML={{__html: ul.innerHTML}}></ul>
+        </div>
+    })
     return (
-        <div dangerouslySetInnerHTML={{__html: props.allRecipeUls[33].outerHTML}} >
-        
+        <div className='ul-group'>
+            {uls}
+            {props.allRecipeUls.length ? <button>Next</button> : ''}
         </div>
     );
 };
